1b08e49db1ec66bd658efbb92ee64d1bb8f773eb
unknown <epqlfepzk@naver.com>
2023-01-18 17:11:49 +0900

    first commit

---
+++ spring_ExceptionHandler/src/main/java/com/springrecipe/sts/SpringRecipe25Application.java
@@ -0,0 +1,13 @@
+package com.springrecipe.sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipe25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecipe25Application.class, args);
+	}
+
+}
