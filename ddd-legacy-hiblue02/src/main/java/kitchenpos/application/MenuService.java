7608ce9247695723d6ec1d7799bb2a15800cbbf8
rrew77@nexpa.co.kr <rrew77@nexpa.co.kr>
2023-09-01 18:00:14 +0900

    Http Client 사용 시 환경변수 설정 및 메뉴 생성 주석 완료

--- ddd-legacy-hiblue02/src/main/java/kitchenpos/application/MenuService.java
+++ ddd-legacy-hiblue02/src/main/java/kitchenpos/application/MenuService.java
@@ -31,25 +31,60 @@ public class MenuService {
     @Transactional
     public Menu create(final Menu request) {
         final BigDecimal price = request.getPrice();
+        /**
+         * Objects.isNull price 가 null 이면 true
+         * price.compareTo 비교  :  price 와 BigDecimal.ZERO 같으면 0  , price가 작으면 -1
+         * 결론 : price 가 마이너스이면 true
+         */
         if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException();
         }
+        /**
+         * Menu에서 메뉴그룹 id 찾아 menuGroup을 찾는다. 없으면 Nosuch 오류
+         */
         final MenuGroup menuGroup = menuGroupRepository.findById(request.getMenuGroupId())
             .orElseThrow(NoSuchElementException::new);
+        /**
+         * 해당 menu에 있는 menu_상품을 선언한다.
+         * 찾은 menu_상품이 null 이면 true , List<MenuProduct> isEmpty(빈 값) 이면 true
+         * 메뉴_상품이 있어야된다.
+         */
         final List<MenuProduct> menuProductRequests = request.getMenuProducts();
+
         if (Objects.isNull(menuProductRequests) || menuProductRequests.isEmpty()) {
             throw new IllegalArgumentException();
         }
+        /**
+         *  메뉴 -> 메뉴 구성 상품 List에서 상품 id를 가져와 Products 선언
+         *  상품 리스트와 메뉴구성상품 의 사이즈가 다르다면 true
+         *  상품 리스트와 메뉴구성상품의 사이즈가 같아 된다.
+         * */
         final List<Product> products = productRepository.findAllByIdIn(
             menuProductRequests.stream()
                 .map(MenuProduct::getProductId)
                 .collect(Collectors.toList())
         );
+
         if (products.size() != menuProductRequests.size()) {
             throw new IllegalArgumentException();
         }
+
+        /**
+         * List<MenuProduct> 값을 생성
+         * BigDecimal 생성 및 초기화
+         * */
         final List<MenuProduct> menuProducts = new ArrayList<>();
         BigDecimal sum = BigDecimal.ZERO;
+
+        /**
+         * 해당 메뉴에 있는 메뉴상품 구성 List를 하나씩 돌려가며
+         * 메뉴_상품구성의 개수를 가져와 quantity에 저장 , quantity가 0 보다 작으면 오류
+         * 메뉴구성상품에서 상품 id 검색해 product 가져옴
+         * 제품 가격(product.getPrice())을 특정 수량('quantity')으로 곱합니다  기존의 sum 값과 새로 계산된 곱한 값을  더한 후에 원래의 'sum' 변수에 할당
+         * 수량만큼의 제품 가격을 계산하고 그 결과를 sum값 과 합쳐서 'sum' 변수에 저장
+         * 메뉴구성상품 변수 선언 -> 값 세팅 상품,상품의 개수를 메뉴구성상품 엔티티 변수에 저장
+         *
+         * */
         for (final MenuProduct menuProductRequest : menuProductRequests) {
             final long quantity = menuProductRequest.getQuantity();
             if (quantity < 0) {
@@ -58,21 +93,32 @@ public class MenuService {
             final Product product = productRepository.findById(menuProductRequest.getProductId())
                 .orElseThrow(NoSuchElementException::new);
             sum = sum.add(
-                product.getPrice()
-                    .multiply(BigDecimal.valueOf(quantity))
+                product.getPrice().multiply(BigDecimal.valueOf(quantity))
             );
             final MenuProduct menuProduct = new MenuProduct();
             menuProduct.setProduct(product);
             menuProduct.setQuantity(quantity);
             menuProducts.add(menuProduct);
         }
+        /**
+         * 상품과 개수 곱하고 List로 돌면서 합한 값이(sum) 메뉴 가격(price) 보다 낮아야된다. 아니면 오류 발생
+         * 메뉴 가격이 더 높아야된다.
+         * */
         if (price.compareTo(sum) > 0) {
             throw new IllegalArgumentException();
         }
+        /**
+         * 메뉴 이름 변수를 선언
+         * 메뉴이름이 null 이거나 또는 메뉴이름에 욕설이 들어가면 안된다.
+         * */
         final String name = request.getName();
         if (Objects.isNull(name) || purgomalumClient.containsProfanity(name)) {
             throw new IllegalArgumentException();
         }
+        /**
+         * 메뉴 엔티티 선언
+         * 메뉴의 값들을 넣어 줘서  메뉴 값을 저장한다.
+         * */
         final Menu menu = new Menu();
         menu.setId(UUID.randomUUID());
         menu.setName(name);
